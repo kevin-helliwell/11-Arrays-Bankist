2d1e7bf60566f4bedccf69a8120f51e4da265d54
kevin-helliwell <kbh789@gmail.com>
2022-01-27 11:13:41 -0500

    updates

--- starter/script.js
+++ starter/script.js
@@ -61,6 +61,29 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements) {
+  containerMovements.innerHTML = '';
+  // .textContent = 0
+
+
+  movements.forEach(function(mov,i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>
+    `;
+
+containerMovements.insertAdjacentHTML('afterbegin', html);
+
+  })
+}
+displayMovements(account1.movements);
+
+// console.log(containerMovements.innerHTML);
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -92,22 +115,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // 2: function(400)
 
 // Map
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-currencies.forEach(function(value, key, map) {
-  console.log(`${key}: ${value}`);
-});
+// currencies.forEach(function(value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
 
-// Set
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function(value, _, map) {
-  console.log(`${value}: ${value}`);
-})
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function(value, _, map) {
+//   console.log(`${value}: ${value}`);
+// })
 
 
 
